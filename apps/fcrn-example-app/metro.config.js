61933c11103e6fe7399a29800d3e19192d2a5912
Cameron Voell <cameronvoell@gmail.com>
2023-11-04 14:09:28 -0700

    fix metro config

--- apps/fcrn-example-app/metro.config.js
+++ apps/fcrn-example-app/metro.config.js
@@ -1,21 +1,24 @@
-import { getDefaultConfig } from "expo/metro-config";
-import { resolve } from "path";
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Learn more: https://docs.expo.dev/guides/monorepos/
+const { getDefaultConfig } = require("@expo/metro-config");
+const path = require("path");
 
-// Find the project and workspace directories
 const projectRoot = __dirname;
-// This can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = resolve(projectRoot, "../..");
+const workspaceRoot = path.resolve(projectRoot, "../..");
 
+// Create the default Metro config
 const config = getDefaultConfig(projectRoot);
 
-// 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
-// 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-  resolve(projectRoot, "node_modules"),
-  resolve(workspaceRoot, "node_modules"),
-];
-// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+if (config.resolver) {
+  // 1. Watch all files within the monorepo
+  config.watchFolders = [workspaceRoot];
+  // 2. Let Metro know where to resolve packages and in what order
+  config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, "node_modules"),
+    path.resolve(workspaceRoot, "node_modules"),
+  ];
+  // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+  config.resolver.disableHierarchicalLookup = true;
+}
 
-export default config;
+module.exports = config;
